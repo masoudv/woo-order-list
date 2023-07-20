480177b1c41f73faafb782709703f6b3c6e5199e
Masoud Vatankhah [sirvav] <techdh.co@gmail.com>
2023-07-20 13:34:31 +0330

    Update woo-orders-list.php
    Fixed some bugs

--- woo-orders-list.php
+++ woo-orders-list.php
@@ -1,40 +1,65 @@
 <?php
 // Customer Order List
-function woo_orders_list(){
-   /**
+function woo_orders_list() {
+ /**
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
  * @version 5.2.0
  */
 defined( 'ABSPATH' ) || exit;
-do_action( 'woocommerce_before_account_orders', $has_orders ); ?>
+$my_orders_columns = apply_filters(
+	'woocommerce_my_account_my_orders_columns',
+	array(
+		'order-number'  => esc_html__( 'Order', 'woocommerce' ),
+		'order-date'    => esc_html__( 'Date', 'woocommerce' ),
+		'order-status'  => esc_html__( 'Status', 'woocommerce' ),
+		'order-total'   => esc_html__( 'Total', 'woocommerce' ),
+		'order-actions' => '&nbsp;',
+	)
+);
 
-<?php if ( $has_orders ) : ?>
+$customer_orders = get_posts(
+	apply_filters(
+		'woocommerce_my_account_my_orders_query',
+		array(
+			// 'numberposts' => $order_count,
+			'meta_key'    => '_customer_user',
+			'meta_value'  => get_current_user_id(),
+			'post_type'   => wc_get_order_types( 'view-orders' ),
+			'post_status' => array_keys( wc_get_order_statuses() ),
+		)
+	)
+);
 
-	<table class="woocommerce-orders-table woocommerce-MyAccount-orders shop_table shop_table_responsive my_account_orders account-orders-table">
+if ( $customer_orders ) : ?>
+
+	<h2><?php echo apply_filters( 'woocommerce_my_account_my_orders_title', esc_html__( 'Recent orders', 'woocommerce' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></h2>
+
+	<table class="shop_table shop_table_responsive my_account_orders">
+
 		<thead>
 			<tr>
-				<?php foreach ( wc_get_account_orders_columns() as $column_id => $column_name ) : ?>
-					<th class="woocommerce-orders-table__header woocommerce-orders-table__header-<?php echo esc_attr( $column_id ); ?>"><span class="nobr"><?php echo esc_html( $column_name ); ?></span></th>
+				<?php foreach ( $my_orders_columns as $column_id => $column_name ) : ?>
+					<th class="<?php echo esc_attr( $column_id ); ?>"><span class="nobr"><?php echo esc_html( $column_name ); ?></span></th>
 				<?php endforeach; ?>
 			</tr>
 		</thead>
 
 		<tbody>
 			<?php
-			foreach ( $customer_orders->orders as $customer_order ) {
+			foreach ( $customer_orders as $customer_order ) :
 				$order      = wc_get_order( $customer_order ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-				$item_count = $order->get_item_count() - $order->get_item_count_refunded();
+				$item_count = $order->get_item_count();
 				?>
-				<tr class="woocommerce-orders-table__row woocommerce-orders-table__row--status-<?php echo esc_attr( $order->get_status() ); ?> order">
-					<?php foreach ( wc_get_account_orders_columns() as $column_id => $column_name ) : ?>
-						<td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-<?php echo esc_attr( $column_id ); ?>" data-title="<?php echo esc_attr( $column_name ); ?>">
+				<tr class="order">
+					<?php foreach ( $my_orders_columns as $column_id => $column_name ) : ?>
+						<td class="<?php echo esc_attr( $column_id ); ?>" data-title="<?php echo esc_attr( $column_name ); ?>">
 							<?php if ( has_action( 'woocommerce_my_account_my_orders_column_' . $column_id ) ) : ?>
 								<?php do_action( 'woocommerce_my_account_my_orders_column_' . $column_id, $order ); ?>
 
 							<?php elseif ( 'order-number' === $column_id ) : ?>
-								<a href="<?php echo esc_url( $order->get_view_order_url() ); ?>">
-									<?php echo esc_html( _x( '#', 'hash before order number', 'woocommerce' ) . $order->get_order_number() ); ?>
+								<a href="#">
+									<?php echo _x( '#', 'hash before order number', 'woocommerce' ) . $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 								</a>
 
 							<?php elseif ( 'order-date' === $column_id ) : ?>
@@ -46,50 +71,16 @@ do_action( 'woocommerce_before_account_orders', $has_orders ); ?>
 							<?php elseif ( 'order-total' === $column_id ) : ?>
 								<?php
 								/* translators: 1: formatted order total 2: total order items */
-								echo wp_kses_post( sprintf( _n( '%1$s for %2$s item', '%1$s for %2$s items', $item_count, 'woocommerce' ), $order->get_formatted_order_total(), $item_count ) );
+								printf( _n( '%1$s for %2$s item', '%1$s for %2$s items', $item_count, 'woocommerce' ), $order->get_formatted_order_total(), $item_count ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 								?>
 
-							<?php elseif ( 'order-actions' === $column_id ) : ?>
-								<?php
-								$actions = wc_get_account_orders_actions( $order );
-
-								if ( ! empty( $actions ) ) {
-									foreach ( $actions as $key => $action ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-										echo '<a href="' . esc_url( $action['url'] ) . '" class="woocommerce-button button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
-									}
-								}
-								?>
 							<?php endif; ?>
 						</td>
 					<?php endforeach; ?>
 				</tr>
-				<?php
-			}
-			?>
+			<?php endforeach; ?>
 		</tbody>
 	</table>
-
-	<?php do_action( 'woocommerce_before_account_orders_pagination' ); ?>
-
-	<?php if ( 1 < $customer_orders->max_num_pages ) : ?>
-		<div class="woocommerce-pagination woocommerce-pagination--without-numbers woocommerce-Pagination">
-			<?php if ( 1 !== $current_page ) : ?>
-				<a class="woocommerce-button woocommerce-button--previous woocommerce-Button woocommerce-Button--previous button" href="<?php echo esc_url( wc_get_endpoint_url( 'orders', $current_page - 1 ) ); ?>"><?php esc_html_e( 'Previous', 'woocommerce' ); ?></a>
-			<?php endif; ?>
-
-			<?php if ( intval( $customer_orders->max_num_pages ) !== $current_page ) : ?>
-				<a class="woocommerce-button woocommerce-button--next woocommerce-Button woocommerce-Button--next button" href="<?php echo esc_url( wc_get_endpoint_url( 'orders', $current_page + 1 ) ); ?>"><?php esc_html_e( 'Next', 'woocommerce' ); ?></a>
-			<?php endif; ?>
-		</div>
-	<?php endif; ?>
-
-<?php else : ?>
-	<div class="woocommerce-message woocommerce-message--info woocommerce-Message woocommerce-Message--info woocommerce-info">
-		<a class="woocommerce-Button button" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>"><?php esc_html_e( 'Browse products', 'woocommerce' ); ?></a>
-		<?php esc_html_e( 'No order has been made yet.', 'woocommerce' ); ?>
-	</div>
-<?php endif; ?>
-
-<?php do_action( 'woocommerce_after_account_orders', $has_orders );
-}
-add_shortcode('woo_orders_list', 'woo_orders_list');
+   <?php endif;
+                     }
+   add_shortcode('woo_orders_list', 'woo_orders_list');
